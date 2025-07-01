3d54726ce23654b2b34cbfc4e4ac01fabcbbdaa1
shahadot786 <shrhossain786@gmail.com>
2025-07-01 18:55:54 +0600

    minor ch

--- app/src/main/java/com/shahadot/doctor_appointment_app/SplashActivity.java
+++ app/src/main/java/com/shahadot/doctor_appointment_app/SplashActivity.java
@@ -1,31 +1,23 @@
 package com.shahadot.doctor_appointment_app;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.shahadot.doctor_appointment_app.databinding.ActivitySplashBinding;
 
+@SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
-    private ActivitySplashBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        ActivitySplashBinding binding = ActivitySplashBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        binding.startBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-            }
+        binding.startBtn.setOnClickListener(v -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
         });
     }
 }
